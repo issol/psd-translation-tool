3c36515d6efc9a7d047a500ff19ee2a1679de615
LerielKim <leriel@glozinc.com>
2024-01-17 11:53:03 +0900

    [Feat] add config export

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
-export function webpack(config, { isServer }) {
-  //   if (!isServer) {
-  //     config.module.rules.push({
-  //       test: /\.worker\.ts$/,
-  //       loader: 'worker-loader',
-  //       options: {
-  //         esModule: true,
-  //       },
-  //     })
-  //   }
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
 
-  return config
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 }
+
+export default nextConfig
